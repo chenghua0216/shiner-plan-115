ba4c934e33e4f3eb5ae074ff06b90ffffdebdef9
chenghua0216 <cheng.hua0216@gmail.com>
2026-03-27 14:42:04 +0800

    Upload vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-    base: '/shiner-plan-115/',
-    plugins: [react()],
+  base: '/shiner-plan-115/',
+  plugins: [react()],
 })
